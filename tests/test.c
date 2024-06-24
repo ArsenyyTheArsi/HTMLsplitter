0d425784509117691a76e3d00529cda96fa296e8
Promkaa <165302870+Promkaa@users.noreply.github.com>
2024-06-25 04:40:25 +0700

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -1,2 +1,33 @@
-#include <function.h>
-#include <assert.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include "msg_split.h"
+#include "functions.h"
+#include <locale.h>
+
+// create temp file 
+void ftemp_create(const char* filename, const char* content) {
+
+    FILE* Ftmp = fopen(filename, "w"); 
+
+    if (Ftmp == NULL) {
+        perror("File opening error");
+        exit(0);
+    }
+
+    fputs(content, Ftmp);
+    fclose(Ftmp);
+}
+
+
+...
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    WithoutTag();
+    longTag();
+    EmptyFile();
+
+    printf("success!");
+
+    return 0;
+}
