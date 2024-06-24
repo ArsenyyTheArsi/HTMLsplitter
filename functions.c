3671de1e62c45f5abf383d02ac15ce13c1079302
Exelenter911 <145780415+Exelenter911@users.noreply.github.com>
2024-06-24 22:28:06 +0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,12 @@
 #include <functions.h>
+
+// Function to add open tags to the start of the next fragment 
+void next_fragment_tags(char output[], char writtenTags[1024][MAX_TAG_LEN], int countTags, int *currentString_len, int *currentChar_index) { 
+    for (int i = 0; i < countTags; i++) { 
+        if (writtenTags[i][1] != '/') { 
+            strcat(output, writtenTags[i]); 
+            *currentString_len += strlen(writtenTags[i]); 
+            *currentChar_index += strlen(writtenTags[i]); 
+        } 
+    } 
+}
