68dc786ba76c34ade9aae45bfd555a0df5412be8
arsithearsi <156746119+ArsenyyTheArsi@users.noreply.github.com>
2024-06-24 22:44:01 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,3 +8,5 @@
 
 void next_fragment_tags(char output[], char writtenTags[1024][MAX_TAG_LEN], int countTags, int *currentString_len, int *currentChar_index);// Function to add open tags to the start of the next fragment
 void make_html_fragment(char *output, int *fragment_number);// Function to create a new HTML fragment
+
+int msg_split(FILE* sourceHTML, int max_len);
