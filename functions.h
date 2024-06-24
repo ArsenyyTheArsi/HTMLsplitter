edafeb50ff7d57e509e379f6c8c94a3d53d8f5eb
arsithearsi <156746119+ArsenyyTheArsi@users.noreply.github.com>
2024-06-24 22:12:12 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,3 +5,6 @@
 #define MAX_TAG_LEN 100
 #define BLOCK_TAGS_NUM 16
 #define MAX_FILE_NAME_LEN 256
+
+void next_fragment_tags(char output[], char writtenTags[1024][MAX_TAG_LEN], int countTags, int *currentString_len, int *currentChar_index);// Function to add open tags to the start of the next fragment
+void make_html_fragment(char *output, int *fragment_number);// Function to create a new HTML fragment
